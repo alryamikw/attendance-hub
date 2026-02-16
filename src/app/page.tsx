0f37220f09ffbfdd576f9859f67cf94f9f1ffd75
alryamikw <atwi.ryami@gmail.com>
2026-02-16 17:56:57 +0300

    Force dynamic rendering to fix navigator error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+export const dynamic = 'force-dynamic';
 import { useState, useEffect, useCallback, useRef } from 'react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
